1237663d239f3fb7305525ff72a81ccd311c83d6
V Mitchell <49798783+V-Mitchell@users.noreply.github.com>
2021-10-11 18:41:21 -0700

    Add files via upload

--- shape_recognition/shape_recognition.cpp
+++ shape_recognition/shape_recognition.cpp
@@ -24,61 +24,85 @@ void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& cont
 
 int main (int argc, char* argv[]) {
 
-    cv::Mat img = cv::imread("/home/victor-m/Pictures/shapes.png");
-    if (img.empty()) {
-        std::cout << "Can't Read Image" << std::endl;
-        return 0;
+    cv::VideoCapture capture(0);
+
+    if (!capture.isOpened()) {
+        std::cout << "Couldn't open camera" << std::endl;
+        return -1;
     }
 
-    cv::Mat gray;
-    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    double camWidth = capture.get(cv::CAP_PROP_FRAME_WIDTH); // get video frame width
+    double camHeight = capture.get(cv::CAP_PROP_FRAME_HEIGHT); // get video frame height
 
-    // Use canny to catch squares with gradient shading
-    cv::Mat bw;
-    cv::Canny(gray, bw, 0, 50, 5);
+    std::cout << "Video Resolution: " << camWidth << " x " << camHeight << std::endl;
 
-    // Find Contours
-    std::vector<std::vector<cv::Point> > contours;
-    cv::findContours(bw.clone(), contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-    std::vector<cv::Point> approx;
-    cv::Mat dst = img.clone();
-
-    for (std::size_t i = 0; i < contours.size(); i++) {
-        // Approximate contour with accuracy proportional to contour perimeter
-        cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(contours[i], true)*0.02, true);
-
-        // skip small & non-convex objects
-        if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx)) continue;
-
-        if (approx.size() == 3) {
-            setLabel(dst, "TRI", contours[i]);
-        } else if (approx.size() >= 4 && approx.size() <= 6) {
-            int vtc = approx.size(); // Number of verticies
-
-            if (vtc == 4)
-                setLabel(dst, "QUAD", contours[i]);
-            else if (vtc == 5)
-                setLabel(dst, "PENTA", contours[i]);
-            else if (vtc ==6) 
-                setLabel(dst, "HEXA", contours[i]);
-
-        } else {
-            // Detect circular objects
-            double area = cv::contourArea(contours[i]);
-            cv::Rect r = cv::boundingRect(contours[i]);
-            int radius = r.width / 2;
-
-            if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 && std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
-                setLabel(dst, "CIR", contours[i]);
-
+    while(true) {
+        cv::Mat raw_frame;
+        bool bSuccess = capture.read(raw_frame);
+        if (!bSuccess) {
+            std::cout << "Camera Disconnected - Cannot read frame" << std::endl;
+            break;
         }
 
-        
+        cv::Mat gray;
+        cv::cvtColor(raw_frame, gray, cv::COLOR_BGR2GRAY);
 
+        // Use canny to catch squares with gradient shading
+        cv::Mat bw;
+        cv::Canny(gray, bw, 0, 50, 5);
+
+        // Find Contours
+        std::vector<std::vector<cv::Point> > contours;
+        cv::findContours(bw.clone(), contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        std::vector<cv::Point> approx;
+        cv::Mat dst = raw_frame.clone();
+
+        for (std::size_t i = 0; i < contours.size(); i++) {
+            // Approximate contour with accuracy proportional to contour perimeter
+            cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(contours[i], true)*0.1, true); // 0.02
+
+            // skip small & non-convex objects
+            if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx)) continue;
+
+            if (approx.size() == 3) {
+                setLabel(dst, "TRI", contours[i]);
+            } else if (approx.size() >= 4 && approx.size() <= 6) {
+                int vtc = approx.size(); // Number of verticies
+                int ctArea = cv::contourArea(contours[i]);
+
+                if (ctArea >= 200) {
+                    std::cout << ctArea << std::endl;
+                    if (vtc == 4)
+                        setLabel(dst, "QUAD", contours[i]);
+                    else if (vtc == 5)
+                        setLabel(dst, "PENTA", contours[i]);
+                    else if (vtc ==6) 
+                        setLabel(dst, "HEXA", contours[i]);
+                }
+
+            } else {
+                // Detect circular objects
+                double area = cv::contourArea(contours[i]);
+                cv::Rect r = cv::boundingRect(contours[i]);
+                int radius = r.width / 2;
+
+                if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 && std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
+                    setLabel(dst, "CIR", contours[i]);
+
+            }
+
+            
+
+        }
+        cv::imshow("contours", bw);
+        cv::imshow("dst", dst);
+
+        if (cv::waitKey(30) == 27) {
+            std::cout << "Exiting..." << std::endl;
+            break;
+        }
     }
-    cv::imshow("src", img);
-    cv::imshow("dst", dst);
-    cv::waitKey(0);
+
     return 0;
 }
